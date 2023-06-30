b6c238ca43a475eece2aa06e91342b72efd48e72
Sam Willis <sam@williscloud.org>
2023-07-01 08:59:20 +1000

    Chipping away at the base settings

--- modules/base/default.nix
+++ modules/base/default.nix
@@ -23,22 +23,52 @@ in {
       description =
         "How line numbers are displayed. none, relative, number, relNumber";
     };
+
+    colourTerm = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Set terminal up for 256 colours";
+    };
+
+    showCursorLine = mkOption {
+      type = with types; bool;
+      default = true;
+      description = "Set to highlight the text line of the cursor";
+    };
+
+    signColumnMode = mkOption {
+      type = with types; enum [ "always" "never" "auto" ];
+      default = "always";
+      description = "How to display the sign column";
+    };
+
+    cmdHeight = mkOption {
+      type = with types; int;
+      default = 1;
+      description = "Height of the command bar";
+    };
+
+    scrollOffsetLines = mkOption {
+      type = with types; int;
+      default = 8;
+      description =
+        "The minimum amount of lines to be above or below the cursor line";
+    };
   };
 
   config = {
     # vim.startPlugins = ["plenary-nvim"];
     vim.luaConfigRC.base = nvim.dag.entryAfter [ "globalsScript" ] ''
       -- Settings that are set for everything
+      vim.opt.scrolloff = ${toString cfg.scrollOffsetLines}
+      vim.opt.cmdheight = ${toString cfg.cmdHeight}
+
       ${optionalString (cfg.leaderKey == "backslash") ''
         vim.g.mapleader = "\\"
       ''}
-
-      ---- Leader Key
       ${optionalString (cfg.leaderKey == "space") ''
         vim.g.mapleader = "<space>"
       ''}
-
-      ---- Line Numbering
        ${
          optionalString (cfg.lineNumberMode == "number" || cfg.lineNumberMode
            == "relNumber") ''
@@ -49,12 +79,30 @@ in {
       (cfg.lineNumberMode == "relative" || cfg.lineNumberMode == "relNumber") ''
         vim.opt.relativenumber = true
       ''}
-
-      ---- Error Bell
+       ${
+         optionalString (cfg.showCursorLine) ''
+           vim.opt.cursorline = true
+         ''
+       }
+       ${
+         optionalString (cfg.signColumnMode == "always") ''
+           vim.opt.signcolumn = "yes"
+         ''
+       }
+       ${
+         optionalString (cfg.signColumnMode == "auto") ''
+           vim.opt.signcolumn = "auto"
+         ''
+       }
        ${
          optionalString
          (cfg.errorBell == "visual" || cfg.errorBell == "both") ''
            vim.opt.visualbell = true
+         ''
+       }
+       ${
+         optionalString (cfg.colourTerm) ''
+           vim.opt.termguicolors = true
          ''
        }
        ${
