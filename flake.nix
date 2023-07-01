8fd05f45732e4d60eae3b5f30da06613a83794d9
Sam Willis <sam@williscloud.org>
2023-07-02 09:15:57 +1000

    Added telescope for treesitter

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A very basic flake";
+  description = "Sam's Modular Neovim Flake 🤭";
 
   inputs = {
     nixpkgs = { url = "github:NixOS/nixpkgs"; };
--- modules/telescope/default.nix
+++ modules/telescope/default.nix
@@ -14,7 +14,10 @@ in {
       "<leader>gf" = ":Telescope live_grep<CR>";
       "<leader>gw" = ":Telescope live_grep<CR>";
       "<leader>gb" = ":Telescope current_buffer_fuzzy_find<CR>";
-    };
+    } // (if config.vim.treesitter.enable then {
+      "<leader>fs" = ":Telescope treesitter<CR>";
+    } else
+      { });
     # TODO: Come back to this
     # // (if config.vim.lsp.enable then {
     #   "gd" = ":Telescope lsp_definitions<CR>";
