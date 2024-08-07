85a66f16e354c816305dc7d293db5172e98e8f8a
samjwillis97 <sam@williscloud.org>
2024-08-07 21:09:48 +1000

    WIP 2

--- modules/languages/typescript/default.nix
+++ modules/languages/typescript/default.nix
@@ -88,25 +88,15 @@ let
     vscode-js-debug-node = {
       package = pkgs.vscode-js-debug;
       dapAdapter = ''
-        require("dap-vscode-js").setup({
-          -- node_path = "${pkgs.nodejs_18}/bin/node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
-          -- debugger_path = "${enabledDebuggerPackages.vscode-js-debug-node}", -- Path to vscode-js-debug installation.
-          debugger_cmd = { "${enabledDebuggerPackages.vscode-js-debug-node}/bin/js-debug" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
-          adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' }, -- which adapters to register in nvim-dap
-          -- log_file_path = "/tmp/dap-log", -- Path for file logging
-          log_file_level = vim.log.levels.DEBUG, -- Logging level for output to file. Set to false to disable file logging.
-          log_console_level = vim.log.levels.DEBUG, -- Logging level for output to console. Set to false to disable console output.
-        })
-
-        -- require("dap").adapters["pwa-node"] = {
-        --   type = "server",
-        --   host = "localhost",
-        --   port = "''${port}",
-        --   executable = {
-        --     command = "${enabledDebuggerPackages.vscode-js-debug-node}/bin/js-debug",
-        --     args = { "''${port}" },
-        --   }
-        -- }
+        require("dap").adapters["pwa-node"] = {
+          type = "server",
+          host = "localhost",
+          port = "''${port}",
+          executable = {
+            command = "${enabledDebuggerPackages.vscode-js-debug-node}/bin/js-debug",
+            args = { "''${port}" },
+          }
+        }
       '';
 
       dapConfig = ''
